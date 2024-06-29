7ec9cf4157f89c1a2c526eff3c5c99ef10abf684
Saumya Shah <131935677+Shah-Saumya@users.noreply.github.com>
2024-06-29 09:35:07 -0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,3 +1,14 @@
+const handler = async (req: Request) => {
+    const path = new URL(req.url).pathname;
+    switch (path) {
+        case "test":
+            return new Response("Hello world");
+
+        default:
+            return new Response("What!!!?");
+    }
+};
+
 Deno.serve({
     port: 8000
 }, handler);
